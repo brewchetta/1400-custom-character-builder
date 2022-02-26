fb284af00b853aaf13e332a268a7ff1b823d74d3
brewchetta <chett.tiller@gmail.com>
2022-02-26 14:51:47 -0500

    more robust capitalize

--- src/utilities.js
+++ src/utilities.js
@@ -1,7 +1,11 @@
 export function capitalize(string) {
-  return string[0].toUpperCase() + string.slice(1).toLowerCase()
+  return toSpaceCase(string).split(" ").map(s => s[0].toUpperCase() + s.slice(1).toLowerCase()).join(" ")
 }
 
 export function toSpinalCase(string) {
   return string.toLowerCase().replace(/[\s_]+/g, "-")
+}
+
+export function toSpaceCase(string) {
+  return string.replace(/[_-]/g, " ")
 }
