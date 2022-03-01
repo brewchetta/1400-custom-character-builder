47dd030b8c04d9429b55f01b378d89cc45696001
brewchetta <chett.tiller@gmail.com>
2022-03-01 15:07:08 -0500

    added special text to ancestries

--- src/data/_ancestriesCore.js
+++ src/data/_ancestriesCore.js
@@ -7,16 +7,19 @@ const ancestries = {
   elfin: {
     name: 'Elfin',
     description: 'Learn 1 spell. You cannot be beguiled or put to sleep by magic.',
+    specialText: 'You cannot be beguiled or put to sleep by magic.',
     spells: 1
   },
   dwarf: {
     name: 'Dwarf',
     description: 'Increase 1 skill. You can see fine in low light and resist the effects of poisons and ale.',
+    specialText: 'You can see fine in low light and resist the effects of poisons and ale.',
     skills: 1
   },
   greenskin: {
     name: 'Greenskin',
     description: 'Increase 1 skill. You can breath underwater and have a nasty bite.',
+    specialText: 'You can breath underwater and have a nasty bite.',
     skills: 1
   }
 }
--- src/data/_ancestriesDraochtlan.js
+++ src/data/_ancestriesDraochtlan.js
@@ -1,27 +1,33 @@
 const ancestries = {
   modernHuman: {
     name: 'Modern Human',
-    description: 'Once per situation, negate the effect of a harm.'
+    description: 'Once per situation, negate the effect of a harm.',
+    specialText: 'Once per situation, negate the effect of a harm.'
   },
   faeTouched: {
     name: 'Fae Touched',
     description: 'You cannot be beguiled or put to sleep by magic.',
+    specialText: 'You cannot be beguiled or put to sleep by magic.'
   },
   stoutDeep: {
     name: 'Stout Deep-Dweller',
-    description: 'You can see fine in low light. Carry 1 additional heavy item without being hindered.'
+    description: 'You can see fine in low light. Carry 1 additional heavy item without being hindered.',
+    specialText: 'You can see fine in low light. Carry 1 additional heavy item without being hindered.'
   },
   smallFolk: {
     name: 'Small Folk',
-    description: 'Once per situation, gain advantage to a roll that is aided by your small size.'
+    description: 'Once per situation, gain advantage to a roll that is aided by your small size.',
+    specialText: 'Once per situation, gain advantage to a roll that is aided by your small size.'
   },
   draconicClaws: {
     name: 'Draconic (Sharp Claws)',
-    description: 'Melee Combat is rolled one level higher when attacking with no weapon.'
+    description: 'Melee Combat is rolled one level higher when attacking with no weapon.',
+    specialText: 'Melee Combat is rolled one level higher when attacking with no weapon.'
   },
   draconicBreath: {
     name: 'Draconic (Dragonsbreath)',
-    description: 'Gain Breath Weapon (d8).'
+    description: 'Gain Breath Weapon (d8).',
+    specialText: 'You have Breath Weapon (d8).'
   }
 }
 
