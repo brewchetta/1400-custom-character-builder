929e0611cd6c83a1e6e5ee7e6efd4b77572b07a5
brewchetta <chett.tiller@gmail.com>
2022-03-14 14:36:38 -0400

    can click on a text input label to focus the element

--- src/shared/FormInput.js
+++ src/shared/FormInput.js
@@ -1,11 +1,26 @@
+import { useRef } from 'react'
+
 function FormInput({name, inputType="text", labelText, onChange, value}) {
+
+  const input = useRef()
+
+  function handleClickLabel() {
+    input.current.focus()
+  }
 
   return (
     <>
 
-      {labelText ? <label htmlFor={name}>{labelText}</label> : null}
+      {
+        labelText
+        ?
+        <label htmlFor={name} onClick={handleClickLabel}>{labelText}</label> 
+        :
+        null
+      }
 
       <input
+        ref={input}
         type={inputType}
         name={name}
         onChange={onChange}
