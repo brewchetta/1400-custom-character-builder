eb53e2c941d8ca616734062e7296daa43fd942b7
brewchetta <chett.tiller@gmail.com>
2022-04-15 23:05:37 -0400

    styling for navbar

--- src/AppNavbar.js
+++ src/AppNavbar.js
@@ -2,10 +2,10 @@ import { Link } from 'react-router-dom'
 
 function AppNavbar(props) {
   return (
-    <div>
+    <div className="grid-columns-medium centered">
 
-      <Link to='/'>Home</Link>
-      <Link to='create-character'>Build New Character</Link>
+      <Link to='/' className="text-black">Home</Link>
+      <Link to='create-character' className="text-black">Build New Character</Link>
 
     </div>
   )
