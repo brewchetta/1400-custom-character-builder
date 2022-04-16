9b2c0df24f98c391ce1dab2edfff1b754f0bcff5
brewchetta <chett.tiller@gmail.com>
2022-04-15 21:31:40 -0400

    character class and heritage should be capitalized

--- src/4-CharacterSheet/CharacterBio.js
+++ src/4-CharacterSheet/CharacterBio.js
@@ -1,5 +1,6 @@
 import CharacterBioEdit from "./CharacterBioEdit"
 import saveIcon from 'assets/images/file-save.png'
+import { capitalize } from 'utilities'
 import { useEditableContext } from 'context/EditableContext'
 import { useCharacterContext } from 'context/CharacterContext'
 
@@ -18,7 +19,7 @@ function CharacterBio() {
 
   return (
     <div>
-      <h2>{name} - {ancestry} {className} <button onClick={() => setEditable(prev => !prev)}>{editable ? <img src={saveIcon} alt="Save Changes" style={{height: '1em'}} /> : '📝'}</button></h2>
+      <h2>{name} - {capitalize(ancestry)} {capitalize(className)} <button onClick={() => setEditable(prev => !prev)}>{editable ? <img src={saveIcon} alt="Save Changes" style={{height: '1em'}} /> : '📝'}</button></h2>
 
       { ancestrySpecial && <p>{ancestrySpecial}</p> }
 
