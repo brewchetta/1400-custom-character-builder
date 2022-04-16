02ef69873a522080accb6951ec0ddfcc4f7fc384
brewchetta <chett.tiller@gmail.com>
2022-04-15 22:20:08 -0400

    removed unnecessary area

--- src/5-EquipmentStore/EquipementStoreContainer.js
+++
@@ -1,9 +0,0 @@
-import { useState, useEffect } from 'react'
-
-function EquipmentStoreContainer({handleAddItem}) {
-  return (
-    <p>Equipment store container (all the various sections) go here</p>
-  )
-}
-
-export default EquipmentStoreContainer
