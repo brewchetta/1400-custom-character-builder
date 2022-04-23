dab5408e0a5f7540d7b587e13435fc679a4944d7
brewchetta <chett.tiller@gmail.com>
2022-04-23 16:18:37 -0400

    gave all items durability

--- src/data/_itemsCore.js
+++ src/data/_itemsCore.js
@@ -2,26 +2,36 @@ export const weapons = {
   dagger: {
     key:'dagger',
     name: 'Dagger',
+    durability: 1,
+    maxDurability: 1,
     tags: ['Light Weapon', 'Weapon', 'One Hand']
   },
   shortSword: {
     key:'shortSword',
     name: 'Short Sword',
+    durability: 1,
+    maxDurability: 1,
     tags: ['Light Weapon', 'Weapon', 'One Hand']
   },
   club: {
     key:'club',
     name: 'Club',
+    durability: 1,
+    maxDurability: 1,
     tags: ['Light Weapon', 'Weapon', 'One Hand']
   },
   shortbow: {
     key:'shortbow',
     name: 'Shortbow',
+    durability: 1,
+    maxDurability: 1,
     tags: ['Light Weapon', 'Weapon', 'Bow', 'One Hand']
   },
   bastardSword: {
     key:'bastardSword',
     name: 'Bastard Sword',
+    durability: 1,
+    maxDurability: 1,
     tags: ['Martial Weapon', 'Weapon', 'Heavy', 'Two Hand'],
     cost: 2,
     special: '+1 to Melee Combat rolls'
@@ -31,6 +41,8 @@ export const weapons = {
     name: 'Greataxe',
     tags: ['Martial Weapon', 'Weapon', 'Heavy', 'Two Hand'],
     cost: 2,
+    durability: 1,
+    maxDurability: 1,
     special: '+1 to Melee Combat rolls'
   },
   warhammer: {
@@ -38,6 +50,8 @@ export const weapons = {
     name: 'Warhammer',
     tags: ['Martial Weapon', 'Weapon', 'Heavy', 'Two Hand'],
     cost: 2,
+    durability: 1,
+    maxDurability: 1,
     special: '+1 to Melee Combat rolls'
   },
   longbow: {
@@ -45,6 +59,8 @@ export const weapons = {
     name: 'Longbow',
     tags: ['Martial Weapon', 'Weapon', 'Heavy', 'Bow', 'Two Hand'],
     cost: 2,
+    durability: 1,
+    maxDurability: 1,
     special: '+1 to Archery rolls'
   },
   crossbow: {
@@ -52,6 +68,8 @@ export const weapons = {
     name: 'Crossbow',
     tags: ['Martial Weapon', 'Weapon', 'Heavy', 'Bow', 'Two Hand'],
     cost: 2,
+    durability: 1,
+    maxDurability: 1,
     special: '+1 to Archery rolls'
   }
 }
@@ -94,56 +112,78 @@ export const tools = {
   crowbar: {
     key:'crowbar',
     name: 'Crowbar',
+    durability: 1,
+    maxDurability: 1,
     tags: ['Tool'],
   },
   fishingRod: {
     key:'fishingRod',
     name: 'Fishing Rod',
+    durability: 1,
+    maxDurability: 1,
     tags: ['Tool'],
   },
   grapplingHook: {
     key:'grapplingHook',
     name: 'Grappling Hook',
+    durability: 1,
+    maxDurability: 1,
     tags: ['Tool'],
   },
   huntingTrap: {
     key:'huntingTrap',
     name: 'Hunting Trap',
+    durability: 1,
+    maxDurability: 1,
     tags: ['Tool'],
   },
   instrument: {
     key:'instrument',
     name: 'Instrument',
+    durability: 1,
+    maxDurability: 1,
     tags: ['Tool'],
   },
   lockpicks: {
     key:'lockpicks',
     name: 'Lockpicks',
+    durability: 1,
+    maxDurability: 1,
     tags: ['Tool'],
   },
   pestleAndMortar: {
     key:'pestleAndMortar',
     name: 'Pestle and Mortar',
+    durability: 1,
+    maxDurability: 1,
     tags: ['Tool'],
   },
   pickaxe: {
     key:'pickaxe',
     name: 'Pickaxe',
+    durability: 1,
+    maxDurability: 1,
     tags: ['Tool'],
   },
   tenFootPole: {
     key:'tenFootPole',
     name: 'Ten Foot Pole',
+    durability: 1,
+    maxDurability: 1,
     tags: ['Tool'],
   },
   shovel: {
     key:'shovel',
     name: 'Shovel',
+    durability: 1,
+    maxDurability: 1,
     tags: ['Tool'],
   },
   tinderbox: {
     key:'tinderbox',
     name: 'Tinderbox',
+    durability: 1,
+    maxDurability: 1,
     tags: ['Tool'],
   }
 }
@@ -152,21 +192,29 @@ export const supplies = {
   ballBearings: {
     key:'ballBearings',
     name: 'Ball Bearings',
+    durability: 1,
+    maxDurability: 1,
     tags: ['Supply'],
   },
   bandages: {
     key:'bandages',
     name: 'Bandages',
+    durability: 1,
+    maxDurability: 1,
     tags: ['Supply'],
   },
   backpack: {
     key:'backpack',
     name: 'Backpack',
+    durability: 1,
+    maxDurability: 1,
     tags: ['Supply']
   },
   chalk: {
     key:'chalk',
     name: 'Chalk',
+    durability: 1,
+    maxDurability: 1,
     tags: ['Supply'],
   },
   ironSpike: {
@@ -179,17 +227,23 @@ export const supplies = {
   fourPersonTent: {
     key:'fourPersonTent',
     name: 'Four Person Tent',
+    durability: 1,
+    maxDurability: 1,
     tags: ['Supply', 'Heavy'],
     cost: 2
   },
   ram: {
     key:'ram',
     name: 'Ram',
+    durability: 1,
+    maxDurability: 1,
     tags: ['Supply', 'Heavy'],
   },
   rope: {
     key:'rope',
     name: 'Rope (30ft)',
+    durability: 1,
+    maxDurability: 1,
     tags: ['Supply'],
   },
   torch: {
@@ -202,11 +256,15 @@ export const supplies = {
   waterskin: {
     key:'waterskin',
     name: 'Waterskin',
+    durability: 1,
+    maxDurability: 1,
     tags: ['Supply']
   },
   wire: {
     key:'wire',
     name: 'Wire (10ft)',
+    durability: 1,
+    maxDurability: 1,
     tags: ['Supply'],
   },
 }
@@ -217,6 +275,8 @@ export const vehicles = {
     name: 'Cart',
     tags: ['Vehicle', 'Heavy'],
     cost: 2,
+    durability: 2,
+    maxDurability: 2,
     special: "Enough room for 2"
   },
   wagon: {
@@ -224,6 +284,8 @@ export const vehicles = {
     name: 'Wagon',
     tags: ['Vehicle', 'Heavy'],
     cost: 4,
+    durability: 4,
+    maxDurability: 4,
     special: "Enough room for 4"
   },
   rowboat: {
@@ -231,6 +293,8 @@ export const vehicles = {
     name: 'Rowboat',
     tags: ['Vehicle', 'Heavy'],
     cost: 3,
+    durability: 3,
+    maxDurability: 3,
     special: "Enough room for 3"
   },
   ship: {
@@ -238,6 +302,8 @@ export const vehicles = {
     name: 'Ship',
     tags: ['Vehicle', 'Heavy'],
     cost: 10,
+    durability: 10,
+    maxDurability: 10,
     special: "Enough room for 10"
   },
 }
@@ -247,6 +313,8 @@ export const animals = {
     key:'smallAnimal',
     name: 'Small Animal Companion',
     tags: ['Animal Companion', 'Small Animal Companion'],
+    durability: 1,
+    maxDurability: 1,
     special: 'A small sized animal companion like a raven, cat, or snake.'
   },
   mediumAnimal: {
@@ -254,6 +322,8 @@ export const animals = {
     name: 'Medium Animal Companion',
     tags: ['Animal Companion', 'Medium Animal Companion'],
     cost: 2,
+    durability: 2,
+    maxDurability: 2,
     special: 'A medium sized animal companion like a hound, falcon, or monkey.'
   },
   largeAnimal: {
@@ -261,6 +331,8 @@ export const animals = {
     name: 'Large Animal Companion',
     tags: ['Animal Companion', 'Large Animal Companion'],
     cost: 3,
+    durability: 3,
+    maxDurability: 3,
     special: 'A large sized animal companion like a horse or mule, can be ridden.'
   },
 }
