8181b2c5597d7d47af60cb8e4048803cca40bfbf
brewchetta <chett.tiller@gmail.com>
2022-04-23 09:45:14 -0400

    added draocthlan skill gain for ancestries (assuming villager)

--- src/data/_ancestriesDraochtlan.js
+++ src/data/_ancestriesDraochtlan.js
@@ -1,33 +1,39 @@
 const ancestries = {
   modernHuman: {
     name: 'Modern Human',
-    description: 'Once per situation, negate the effect of a harm.',
-    specialText: 'Once per situation, negate the effect of a harm.'
+    description: 'Once per situation, negate the effect of a harm. Increase two skills at once.',
+    specialText: 'Once per situation, negate the effect of a harm. Increase two skills at once.',
+    skills: 2
   },
   faeTouched: {
     name: 'Fae Touched',
-    description: 'You cannot be beguiled or put to sleep by magic.',
-    specialText: 'You cannot be beguiled or put to sleep by magic.'
+    description: 'You cannot be beguiled or put to sleep by magic. Increase two skills at once.',
+    specialText: 'You cannot be beguiled or put to sleep by magic. Increase two skills at once.',
+    skills: 2
   },
   stoutDeep: {
     name: 'Stout Deep-Dweller',
-    description: 'You can see fine in low light. Carry 1 additional heavy item without being hindered.',
-    specialText: 'You can see fine in low light. Carry 1 additional heavy item without being hindered.'
+    description: 'You can see fine in low light. Carry 1 additional heavy item without being hindered. Increase two skills at once.',
+    specialText: 'You can see fine in low light. Carry 1 additional heavy item without being hindered. Increase two skills at once.',
+    skills: 2
   },
   smallFolk: {
     name: 'Small Folk',
-    description: 'Once per situation, gain advantage to a roll that is aided by your small size.',
-    specialText: 'Once per situation, gain advantage to a roll that is aided by your small size.'
+    description: 'Once per situation, gain advantage to a roll that is aided by your small size. Increase two skills at once.',
+    specialText: 'Once per situation, gain advantage to a roll that is aided by your small size. Increase two skills at once.',
+    skills: 2
   },
   draconicClaws: {
     name: 'Draconic (Sharp Claws)',
-    description: 'Melee Combat is rolled one level higher when attacking with no weapon.',
-    specialText: 'Melee Combat is rolled one level higher when attacking with no weapon.'
+    description: 'Melee Combat is rolled one level higher when attacking with no weapon. Increase two skills at once.',
+    specialText: 'Melee Combat is rolled one level higher when attacking with no weapon. Increase two skills at once.',
+    skills: 2
   },
   draconicBreath: {
     name: 'Draconic (Dragonsbreath)',
-    description: 'Gain Breath Weapon (d8).',
-    specialText: 'You have Breath Weapon (d8).'
+    description: 'Gain Breath Weapon (d8). Increase two skills at once.',
+    specialText: 'You have Breath Weapon (d8). Increase two skills at once.',
+    skills: 2
   }
 }
 
