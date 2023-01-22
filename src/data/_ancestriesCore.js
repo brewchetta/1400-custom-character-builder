74bfe3718103f5cfb61fcdb0d7fd3bc59e8ba175
brewchetta <chett.tiller@gmail.com>
2023-01-22 11:51:02 -0500

    created an ancestries expanded section and removed greenskin ancestry

--- src/data/_ancestriesCore.js
+++ src/data/_ancestriesCore.js
@@ -4,8 +4,8 @@ const ancestries = {
     description: 'Increase 2 skills at once.',
     skills: 2
   },
-  elfin: {
-    name: 'Elfin',
+  elf: {
+    name: 'Elf',
     description: 'Learn 1 spell. You cannot be beguiled or put to sleep by magic.',
     specialText: 'You cannot be beguiled or put to sleep by magic.',
     spells: 1
@@ -14,12 +14,6 @@ const ancestries = {
     name: 'Dwarf',
     description: 'Increase 1 skill. You can see fine in low light and resist the effects of poisons and ale.',
     specialText: 'You can see fine in low light and resist the effects of poisons and ale.',
-    skills: 1
-  },
-  greenskin: {
-    name: 'Greenskin',
-    description: 'Increase 1 skill. You can breath underwater and have a nasty bite.',
-    specialText: 'You can breath underwater and have a nasty bite.',
     skills: 1
   }
 }
---
+++ src/data/_ancestriesExpanded.js
@@ -0,0 +1,28 @@
+const ancestries = {
+  orc: {
+    name: 'Orc',
+    description: 'Increase 1 skill. If you would be harmed, roll a d20. On a 10 or higher, you shrug off the injury.',
+    specialText: 'If you would be harmed, roll a d20. On a 10 or higher, you shrug off the injury.',
+    skills: 1
+  },
+  goblin: {
+    name: 'Goblin',
+    description: 'Increase 1 skill. You roll a d6 instead of a d4 when hindered unless you wear or carry any heavy items.',
+    specialText: 'You roll a d6 instead of a d4 when hindered unless you wear or carry any heavy items.',
+    skills: 1
+  },
+  devilkin: {
+    name: 'Devilkin',
+    description: 'Learn 1 spell. You are immune to fire and cold.',
+    specialText: 'You are immune to fire and cold.',
+    spells: 1
+  },
+  centaur: {
+    name: 'Centaur',
+    description: 'Learn 1 skill. You can carry an additional heavy item.',
+    specialText: 'You can carry an additional heavy item.',
+    skills: 1
+  },
+}
+
+export default ancestries
