6e86b626adde45f888ce915298c7b00abcf30793
brewchetta <chett.tiller@gmail.com>
2023-01-21 21:39:51 -0500

    added tooltips for each equipment category

--- src/5-EquipmentStore/EquipmentStoreCategory.js
+++ src/5-EquipmentStore/EquipmentStoreCategory.js
@@ -1,4 +1,6 @@
 import EquipmentStoreItem from "./EquipmentStoreItem"
+import HelpButton from 'shared/HelpButton'
+import { rulesGear } from 'data/rules'
 import { capitalize } from 'utilities'
 
 function EquipmentStoreCategory({ name, items }) {
@@ -7,10 +9,20 @@ function EquipmentStoreCategory({ name, items }) {
     <EquipmentStoreItem key={ item.key } item={ item } />
   ) )
 
+  const renderedHelpButton = (
+    name === "weapons" ? <HelpButton info={rulesGear['weapon descriptions']} />
+    : name === "armours" ? <HelpButton info={rulesGear['armour descriptions']} />
+    : name === "tools" ? <HelpButton info={rulesGear['tool descriptions']} />
+    : name === "supplies" ? <HelpButton info={rulesGear['supply descriptions']} />
+    : name === "vehicles" ? <HelpButton info={rulesGear['vehicle descriptions']} />
+    : name === "animals" ? <HelpButton info={rulesGear['animal companion descriptions']} />
+    : <HelpButton info={"Could not find information for that..."} />
+  )
+
   return (
     <div>
 
-      <h3>{ capitalize( name ) }</h3>
+      <h3>{ capitalize( name ) } {renderedHelpButton}</h3>
 
       <table>
         <tbody>
