a6b296ee552e1228e774fe76e819ef35b5916552
brewchetta <chett.tiller@gmail.com>
2023-01-21 21:39:17 -0500

    further shaped data for rulesGear

--- src/data/rules/_rulesGear.js
+++ src/data/rules/_rulesGear.js
@@ -6,22 +6,17 @@ const rulesGear = {
     `All items cost one GP (gold piece) unless its cost is otherwise mentioned. You can usually get more GP by finishing adventures.`
   ],
   management: [
-    `If a character's equipment breaks, click the checkmark to show it's currently been broken. You may replace or repair equipment at the storyteller's behest.`,
-    `You can find areas to add or delete equipment when editing your character.`
+    `If a character's equipment loses durability, click on one of its checkmarks. If it breaks it cannot be used.`,
+    `You may replace or repair equipment at the storyteller's discretion.`,
+    `You can add or delete equipment when editing your character. To edit your character, click the scroll button near your character's name.`
   ],
   'common descriptions': {
     heavy: `A character can only carry one heavy item at a time unless a more specific rule says they can carry more. If a character attempts to carry more than one, they may be hindered in their rolls.`,
     'one hand': `Characters can hold two one handed items at a time.`,
     'two hand': `Characters can hold only one two handed item at a time.`,
+    weapon: `This item is a weapon. Without a weapon, you cannot fight enemies in combat challenges.`,
     armour: `When a character would be injured, a piece of armour is instead damaged. Mark off one piece of its durability.`,
     worn: `Characters must wear this item to gain its benefits. Only one piece of armour can be worn at a time.`,
-    weapon: `This item is a weapon. Without a weapon, you cannot fight enemies in combat challenges.`,
-    'light weapon': `A smaller one handed weapon that anyone can pick up and use.`,
-    'martial weapon': `A heavy weapon that grants a bonus to certain types of combat rolls.`,
-    tool: `An item with a variety of uses.`,
-    supply: `An item that may be destroyed or consumed as they're used. They can be refilled or repaired at the storyteller's discretion.`,
-    vehicle: `Can be used to more easily move from location to location. Vehicles can only transport a certain number of characters and can only be carried at the storyteller's discretion.`,
-    'animal companion': `Act as characters in their own right. Animal companions will follow your commands but may rebel or flee at the storyteller's discretion.`,
   },
   'armour descriptions': {
     armour: `When a character would be injured and they're wearing armour that still has durability, a piece of armour of their choice is damaged instead. Mark off one durability for it.`,
@@ -36,10 +31,18 @@ const rulesGear = {
     bow: `This weapon can attack enemies at a range. Characters always have enough ammunition unless they have lost it as part of the risk in a challenge.`,
     heavy: `A character can only carry one heavy item at a time unless a more specific rule says they can carry more. If a character attempts to carry more than one, they may be hindered in their rolls.`,
   },
-  'tool descriptions': `An item with a variety of uses.`,
-  'supply descriptions': `An item that may be destroyed or consumed as they're used or as part of a risk. They can be refilled or repaired at the storyteller's discretion.`,
-  'vehicle descriptions':  `Can be used to more easily move from location to location. Vehicles can only transport a certain number of characters and can only be carried at the storyteller's discretion.`,
-  'animal companion descriptions': `Act as characters in their own right. Animal companions will follow your commands but may rebel or flee at the storyteller's discretion.`
+  'tool descriptions': {
+    tool: `An item with a variety of uses.`
+  },
+  'supply descriptions': {
+    supply: `An item that may be destroyed or consumed as they're used or as part of a risk. They can be refilled or repaired at the storyteller's discretion.`
+  },
+  'vehicle descriptions':  {
+    vehicle: `Can be used to more easily move from location to location. Vehicles can only transport a certain number of characters and can only be carried at the storyteller's discretion.`
+  },
+  'animal companion descriptions': {
+    'animal companion': `Act as characters in their own right. Animal companions will follow your commands but may act of their own accord, be injured, or die at the storyteller's discretion.`
+  }
 
 }
 
