c6e58ac9cf705076a7b07ed9d5e3259035d965ca
brewchetta <chett.tiller@gmail.com>
2023-02-15 20:49:31 -0500

    cleaned up various warning messages in console

--- src/router-pages/CharacterSheetPage/EquipmentStore/EquipmentStoreSpells.js
+++ src/router-pages/CharacterSheetPage/EquipmentStore/EquipmentStoreSpells.js
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react'
+import { useState } from 'react'
 import { useCharacterContext } from 'context/CharacterContext'
 import EquipmentStoreSpellItem from './EquipmentStoreSpellItem'
 import HelpButton from 'shared/HelpButton'
