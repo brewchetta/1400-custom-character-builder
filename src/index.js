3b06f08a5d118454c9ef1ef5b6df145d5669921d
brewchetta <chett.tiller@gmail.com>
2023-02-23 15:08:52 -0500

    removed strict mode

--- src/index.js
+++ src/index.js
@@ -5,10 +5,8 @@ import 'style/index.scss';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root')
 );
