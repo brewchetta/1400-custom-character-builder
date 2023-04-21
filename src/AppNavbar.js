2eb6bf112c4686d0e0543a8d8b910287d6f2f383
brewchetta <chett.tiller@gmail.com>
2023-04-21 13:12:11 -0400

    slight changes to navbar

--- src/AppNavbar.js
+++ src/AppNavbar.js
@@ -4,9 +4,9 @@ function AppNavbar(props) {
   return (
     <div className="grid-columns-medium centered margin-bottom-medium">
 
-      <Link to='/' className="text-black no-decoration swatch-hover-background-sky-blue">Home</Link>
-      <Link to='create-character' className="text-black no-decoration swatch-hover-background-orange">Build New Character</Link>
-      <Link to='rulebook' className="text-black no-decoration swatch-hover-background-green">Rulebook</Link>
+      <Link to='/' className="text-black no-decoration swatch-hover-background-sky-blue margin-bottom-medium">Home</Link>
+      <Link to='create-character' className="text-black no-decoration swatch-hover-background-orange margin-bottom-medium">New Character</Link>
+      <Link to='rulebook' className="text-black no-decoration swatch-hover-background-green margin-bottom-medium">Rulebook</Link>
 
     </div>
   )
