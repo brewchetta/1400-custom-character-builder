3dbdee3e51ebd00fcd74996db507ebfa818417b8
brewchetta <chett.tiller@gmail.com>
2023-04-21 09:21:59 -0400

    rulebook now iterates through all rules in the rulePlay section

--- src/router-pages/RulebookPage/RulebookMain.js
+++ src/router-pages/RulebookPage/RulebookMain.js
@@ -4,30 +4,16 @@ import RulePanel from "./RulePanel"
 
 function TutorialMain(props) {
 
-  const navOptions = ['play', 'rolling', 'spells', 'rituals', 'load', 'advancement', 'defense', 'harm', 'game mastery']
+  const navOptions = Object.keys(rules)
+
+  const renderRulePanels = navOptions.map(rule => <RulePanel key={rule} rule={rules[rule]} nav={rule} />)
 
   return (
     <div className="">
 
       <RulebookNav navOptions={navOptions} nav={''} />
 
-      <RulePanel rule={rules.play} nav={'play'} />
-
-      <RulePanel rule={rules.rolling} nav={'rolling'} className="dice-background" />
-
-      <RulePanel rule={rules.spells} nav={'spells'} />
-
-      <RulePanel rule={rules.rituals} nav={'rituals'} />
-
-      <RulePanel rule={rules.load} nav={'load'} />
-
-      <RulePanel rule={rules.advancement} nav={'advancement'} />
-
-      <RulePanel rule={rules.defence} nav={'defense'} />
-
-      <RulePanel rule={rules.harm} nav={'harm'} />
-
-      <RulePanel rule={rules['game mastery']} nav={'game mastery'} />
+      {renderRulePanels}
 
     </div>
   )
