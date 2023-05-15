eee9609f2c671b84beb93b3041c6a714f46bbb63
brewchetta <chett.tiller@gmail.com>
2023-05-15 17:25:57 -0400

    fixed typo

--- src/router-pages/CharacterSheetPage/EquipmentStore/EquipmentStoreSpells.js
+++ src/router-pages/CharacterSheetPage/EquipmentStore/EquipmentStoreSpells.js
@@ -5,7 +5,7 @@ import HelpButton from 'shared/HelpButton'
 import { capitalize } from 'utilities'
 import CollapsibleWrapper from 'shared/CollapsibleWrapper'
 import CollapsibleOpenButton from 'shared/CollapsibleOpenButton'
-import { rulePlay } from 'data/rules'
+import { rulesPlay } from 'data/rules'
 
 // used for both spells and rituals, category determines the difference
 function EquipmentStoreSpells({ category = "spells", spells = [] }) {
@@ -26,7 +26,7 @@ function EquipmentStoreSpells({ category = "spells", spells = [] }) {
   ) )
 
   const renderedHelpButton = (
-    category === "spells" ? <HelpButton info={rulePlay.spells} />
+    category === "spells" ? <HelpButton info={rulesPlay.spells} />
     : category === "rituals" ? <HelpButton info={rulesPlay.rituals} />
     : <HelpButton info={"Could not find information for that..."} />
   )
