9cd4ebecd84f07e9b011996ba327d8c6f96ffd9a
brewchetta <chett.tiller@gmail.com>
2024-10-01 22:44:14 -0400

    rules updates and tweaks to langauge

--- src/data/rules/_rulesPlay.js
+++ src/data/rules/_rulesPlay.js
@@ -9,7 +9,7 @@ const rulesPlay = {
   dice: [
     `This system requires six differently sized dice determined by the number of sizes.`,
     `A d4 is a four sided dice, a d6 is a six sided, etc.`,
-    `The d20 is only used in very special circumstances, most often for a luck roll.`,
+    `The d20 is only used in special circumstances, most often for a luck roll.`,
   ],
   rolling: [
     `When faced with a challenge, roll a d6 skill die - higher with a relevant skill.`,
@@ -26,15 +26,17 @@ const rulesPlay = {
     `The storyteller can return players their luck dice for good roleplaying, generosity, sacrifice, or any other reason.`
   ],
   spells: [
-    'A spell\'s name describes it. Describe the spell when you cast it and the storyteller determines its limitations.',
-    'The storyteller may impose a challenge if the spell presents a risk. This can include overexerting your character\'s magic and losing the spell for a while.',
+    'Spells have names. When using a spell, describe how it\'s cast and its intended effect.',
+    'The storyteller might impose a challenge if the spell presents a risk or a powerful effect. This can include overexerting your character\'s spell.',
     'If the spell has any risk, the roll is Spellcasting',
-    'You can disable a spell by clicking the checkmark.'
+    'You can disable an exhausted spell by clicking the checkmark.',
+    'The storyteller determines based on success and intention how the effect manifests.',
   ],
   rituals: [
-    'A ritual takes ten minutes in game to cast and uses up a magical supply with the ritual tag.',
-    'The blood of a recently killed person, or the sacrifice of a rare animal or plant can be substituted for a magical supply.',
-    'You roll a spellcasting challenge to see how successfully you prepared the ritual.',
+    'Rituals take ten minutes to cast and use a magical supply with the ritual tag.',
+    'The blood of a recently killed person, or the sacrifice of a rare animal or plant can be substituted.',
+    'Multiple people can take part. Each rolls a spellcasting challenge. The highest roll determines success or failure.',
+    'Each participant in the ritual becomes "connected" and shares one truthful answer to another player\'s question.',
   ],
   load: [
     `Carry as much as makes sense, but more than one heavy item may hinder you at times.`
@@ -43,13 +45,15 @@ const rulesPlay = {
     `After an adventure, increase a skill by editing your character (none→d8→d10→d12). Your storyteller may award you gold as well (usually 1d6 to each member).`
   ],
   defence: [
-    `Instead of being injured, your worn armor loses a point of durability.`
+    `Instead of being injured, your worn armor loses a point of durability. Alternately, one of your items breaks or some other ill effect happens.`,
+    `In town it takes one gold to repair a broken or disabled item.`
   ],
   hindrance: [
     `If your character is hindered, either in the short or long term, roll with a d4 instead of a larger dice.`
   ],
   harm: [
     `Injuries hinder (roll with a d4 for all skill challenges), and heal with time, magic, or potions.`,
+    `Exhaustion, encumbrance, low morale, or other effects can also be considered "injuries".`,
     `If killed, make a new character to be introduced ASAP. Favour inclusion over realism.`
   ],
   'game mastery': [
