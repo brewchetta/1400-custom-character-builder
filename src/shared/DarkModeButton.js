9cea077ea1796494037422ffb23891b96d94c47e
brewchetta <chett.tiller@gmail.com>
2024-10-21 17:32:54 -0400

    versions 1 and 2 of darkmode button

--- src/shared/DarkModeButton.js
+++ src/shared/DarkModeButton.js
@@ -1,45 +1,30 @@
+// VERSION 1
+
+// function DarkModeButton({ darkMode, setDarkMode }) {
+
+//     return (
+//         <button className="darkmode-button" onClick={() => setDarkMode(!darkMode)}>
+
+//             <div/>
+//             <span>☀</span>
+//             <span>☾</span>
+
+//         </button>
+//     )
+
+// }
+
+// VERSION 2
+
 function DarkModeButton({ darkMode, setDarkMode }) {
 
-
-    const mainStyles = {
-        background: darkMode ? 'white' : 'black',
-        color: darkMode ? 'black' : 'white',
-        zIndex: 2,
-        transition: "background 0.2s linear, color 0.2s linear",
-        width: "4em",
-        height: '1.5em',
-        position: "relative",
-    }
-
-    const sunStyle = {
-        position: 'absolute',
-        left: '0.25em',
-        top: 0
-    }
-
-    const moonStyle = {
-        position: 'absolute',
-        left: '3em',
-        top: 0
-    }
-
-    const highlightStyles = {
-        position: 'absolute',
-        width: '1.2em',
-        height: '1.2em',
-        border: darkMode ? 'solid black 2px' : 'solid white 2px',
-        top: 0,
-        left: darkMode ? '0em' : '2.5em',
-        borderRadius: '100%',
-        transition: 'left 0.2s'
-    }
+    const className = darkMode ? "darkmode-button night" : "darkmode-button day"
 
     return (
-        <button onClick={() => setDarkMode(!darkMode)} style={mainStyles}>
+        <button className={className} onClick={() => setDarkMode(!darkMode)}>
 
-            <span style={sunStyle}>☀</span>
-            <span style={moonStyle}>☾</span>
-            <div style={highlightStyles} />
+            <span>★</span>
+            <span>☀</span>
 
         </button>
     )
@@ -48,4 +33,4 @@ function DarkModeButton({ darkMode, setDarkMode }) {
 
 export default DarkModeButton
 
-//  ☀ ☾
+//  ☀ ☾ ★
