165b2c5f7efaeb8a3322daac392d4fc8e2c6fe7b
brewchetta <chett.tiller@gmail.com>
2024-10-22 14:59:05 -0400

    fix for overwhelming drawer mask

--- src/shared/DrawerModal.js
+++ src/shared/DrawerModal.js
@@ -2,7 +2,7 @@ function DrawerModal({className, style, children, isOpen}) {
 
   return (
     <>
-      <div className="drawer-mask"/>
+      { isOpen ? <div className="drawer-mask"/> : null }
 
       <div className={`${className} bottom-drawer ${isOpen ? "open" : "closed"}`} style={style}>
         {children}
