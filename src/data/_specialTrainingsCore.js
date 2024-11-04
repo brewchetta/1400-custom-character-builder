ec3f843abc26de6bd8d0665c11f38ff4184f727a
brewchetta <chett.tiller@gmail.com>
2024-11-04 15:39:24 -0500

    added specialTrainings file

---
+++ src/data/_specialTrainingsCore.js
@@ -0,0 +1,29 @@
+// specialized trainings for characters
+const specialTrainings = {
+    brawler: {
+        name: "Brawler",
+        description: "You aren't hindered and get +1 to Melee Combat when fighting without weapons."
+    },
+    lucky: {
+        name: "Lucky",
+        description: "When you roll your luck dice, you don't use up your luck on a roll of 15+"
+    },
+    magicalMusician: {
+        name: "Magical Musician",
+        description: "You can use Performance instead of Spellcasting when casting spells. This only works if you sing or use an instrument as part of casting the spell."
+    },
+    packMule: {
+        name: "Pack Mule",
+        description: "You can carry an additional Heavy item."
+    },
+    likeable: {
+        name: "Likeable",
+        description: "Gain a +1 to Persuasion and Deception"
+    },
+    ritualist: {
+        name: "Ritualist",
+        description: "If you are part of a ritual, a roll of 6+ doesn't use up the magical ritual items."
+    }
+}
+
+export default specialTrainings
